332e28ca7da493ad9922e353a76632247f2692ef
繁星 <1187598002@qq.com>
2025-09-04 19:43:06 +0800

    初步对ThreadCache的bug修复及测试

--- ThreadCache.cpp
+++ ThreadCache.cpp
@@ -2,6 +2,7 @@
 // Created by stars on 2025/9/4.
 //
 
+#include <cstdlib>
 #include "ThreadCache.h"
 
 namespace MemoryPool
@@ -9,6 +10,9 @@ namespace MemoryPool
     thread_local ThreadCache* ThreadCache::cache = nullptr;
 
     ThreadCache::ThreadCache() {
+        for (auto& poolGroup : pools) {
+            poolGroup = new MemoryPools();
+        }
     }
 
     ThreadCache::~ThreadCache() {
@@ -25,34 +29,34 @@ namespace MemoryPool
         return cache;
     }
 
-    template <typename T>
-    /**
-     * 分配内存空间用于存储类型为T的对象
-     * @return 返回指向分配内存的指针，如果分配失败则返回nullptr
-     */
-    T* ThreadCache::allocate(){
-        // 计算需要的槽大小，通过将类型T的大小向上取整到8的倍数并除以8
-        size_t slotSize = (sizeof(T) + 7) / 8 - 1;
-        // 从缓存中获取对应槽大小的内存池
-        MemoryPool* pool = getCache()->pools[slotSize]->firstPool;
-        // 用于存储分配对象的指针
-        void* obj = nullptr;
-        // 尝试从当前内存池分配空间
-        if(pool != nullptr){
-            if ((obj = pool->allocate()) != nullptr) return (T*)obj;
-            // 如果当前内存池分配失败，则尝试后续的内存池
-            while(pool->nextPool != nullptr){
-                if ((obj = pool->allocate()) != nullptr) return (T*)obj;
-                pool = pool->nextPool;
-            }
-        }
-        // 如果所有现有内存池都分配失败，则请求一个新的内存池
-        pool = allocatePool(slotSize);
-        // 尝试从新分配的内存池中分配空间
-        if ((obj = pool->allocate()) != nullptr) return (T*)obj;
-        // 如果所有尝试都失败，返回nullptr
-        return nullptr;
-    }
+    // template <typename T>
+    // /**
+    //  * 分配内存空间用于存储类型为T的对象
+    //  * @return 返回指向分配内存的指针，如果分配失败则返回nullptr
+    //  */
+    // T* ThreadCache::allocate(){
+    //     // 计算需要的槽大小，通过将类型T的大小向上取整到8的倍数并除以8
+    //     size_t slotSize = (sizeof(T) + 7) / 8 - 1;
+    //     // 从缓存中获取对应槽大小的内存池
+    //     MemoryPool* pool = getCache()->pools[slotSize]->firstPool;
+    //     // 用于存储分配对象的指针
+    //     void* obj = nullptr;
+    //     // 尝试从当前内存池分配空间
+    //     if(pool != nullptr){
+    //         if ((obj = pool->allocate()) != nullptr) return (T*)obj;
+    //         // 如果当前内存池分配失败，则尝试后续的内存池
+    //         while(pool->nextPool != nullptr){
+    //             if ((obj = pool->allocate()) != nullptr) return (T*)obj;
+    //             pool = pool->nextPool;
+    //         }
+    //     }
+    //     // 如果所有现有内存池都分配失败，则请求一个新的内存池
+    //     pool = allocatePool(slotSize);
+    //     // 尝试从新分配的内存池中分配空间
+    //     if ((obj = pool->allocate()) != nullptr) return (T*)obj;
+    //     // 如果所有尝试都失败，返回nullptr
+    //     return nullptr;
+    // }
 
     template <typename T>
     void ThreadCache::deallocate(T* ptr) {
@@ -83,7 +87,25 @@ namespace MemoryPool
         }
     }
 
-    MemoryPool* ThreadCache::allocatePool(size_t slotSize) {}
+    MemoryPool* ThreadCache::allocatePool(size_t slotSize)
+    {
+        size_t slotNum = slotSize / ALIGN - 1;
+        char* slot = static_cast<char*>(std::malloc(slotSize * EACHPOOL_SLOT_NUM));
+        if (slot == nullptr) return nullptr;
+        MemoryPool* pool = new MemoryPool(slot, slotSize * EACHPOOL_SLOT_NUM, slotSize);
+        MemoryPool* temp = getCache()->pools[slotNum]->firstPool;
+        if(temp == nullptr){
+            getCache()->pools[slotNum]->firstPool = pool;
+        }else{
+            while(temp->nextPool != nullptr){
+                temp = temp->nextPool;
+            }
+            temp->nextPool = pool;
+            pool->prevPool = temp;
+        }
+        getCache()->pools[slotNum]->poolsmap[slot] = pool;
+        return pool;
+    }
 
     void ThreadCache::deallocatePool(MemoryPool* pool) {}
 
--- ThreadCache.h
+++ ThreadCache.h
@@ -38,14 +38,38 @@ namespace MemoryPool
         static ThreadCache* getCache();
 
         template <typename T>
-        T* allocate();
+        static T* allocate(){
+            // 计算需要的槽大小，通过将类型T的大小向上取整到8的倍数并除以8
+            size_t slotNum = (sizeof(T) + ALIGN - 1) / ALIGN - 1;
+            size_t slotSize = (slotNum + 1) * ALIGN;
+            // 从缓存中获取对应槽大小的内存池
+            MemoryPool* pool = getCache()->pools[slotNum]->firstPool;
+            // 用于存储分配对象的指针
+            void* obj = nullptr;
+            // 尝试从当前内存池分配空间
+            if(pool != nullptr){
+                if ((obj = pool->allocate()) != nullptr) return (T*)obj;
+                // 如果当前内存池分配失败，则尝试后续的内存池
+                while(pool->nextPool != nullptr){
+                    if ((obj = pool->allocate()) != nullptr) return (T*)obj;
+                    pool = pool->nextPool;
+                }
+            }
+            // 如果所有现有内存池都分配失败，则请求一个新的内存池
+            pool = allocatePool(slotSize);
+            // 尝试从新分配的内存池中分配空间
+            if ((obj = pool->allocate()) != nullptr) return (T*)obj;
+            // 如果所有尝试都失败，返回nullptr
+            return nullptr;
+        }
+
 
         template <typename T>
-        void deallocate(T* ptr);
+        static void deallocate(T* ptr);
 
-        MemoryPool* allocatePool(size_t slotSize);
+        static MemoryPool* allocatePool(size_t slotSize);
 
-        void deallocatePool(MemoryPool* pool);
+        static void deallocatePool(MemoryPool* pool);
     };
 } // MemoryPool
 
