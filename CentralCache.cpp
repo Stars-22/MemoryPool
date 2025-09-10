e78e5a07d8124164b151238394255c421d711b39
suse955 <105738130+suse955@users.noreply.github.com>
2025-09-10 17:27:18 +0800

    没写完centralcache，而且有问题

---
+++ CentralCache.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LinJun on 2025/9/10.
+//
+
+#include "CentralCache.h"
---
+++ CentralCache.h
@@ -0,0 +1,16 @@
+//
+// Created by LinJun on 2025/9/10.
+//
+
+#ifndef CENTRALCACHE_H
+#define CENTRALCACHE_H
+
+
+
+class CentralCache {
+
+};
+
+
+
+#endif //CENTRALCACHE_H
